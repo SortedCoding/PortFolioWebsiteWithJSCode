45e04c82d5d0d2437d472eb97d2d96eecc1ad67b
SortedCoding <rautx158@gmail.com>
2024-09-02 18:38:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var typed = new Typed(".typewriter-text ",
+    {
+        strings: [" ",
+            "Web Developer",
+            " ",
+            "Web Designer",
+            " ",
+            "Freelancer",
+            " ",
+            "Travel Blogger",
+            " ",
+            " Youtuber"
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true,
+        backDelay: 1000
+        
+    }
+)
